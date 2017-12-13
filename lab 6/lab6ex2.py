bf81415d178434175b0c5ff1b5b299ebb6e560d8
basel19-meet <basel19@meet.mit.edu>
2017-12-13 19:14:21 +0200

    ece

---
+++ lab 6/lab6ex2.py
@@ -0,0 +1,39 @@
+from turtle import *
+import random
+import math
+
+class rekt(Turtle):
+	def __init__(self,X1,X2,Y1,Y2):
+		Turtle.__init__(self)
+		self.width = math.fabs(X1 - X2)
+		self.height = math.fabs(Y1-Y2)
+
+		self.X1 = X1
+		self.X2 = X2
+		self.Y1 = Y1
+		self.Y2 = Y2
+	def getrekt(self):
+		for i in range(2):
+			self.forward(self.width)
+			self.left(90)
+			self.forward(self.height)
+			self.left(90)
+	def coco(self,rect_1,rect_2):
+		if (rect_1.Y2 >= rect_2.Y1 and rect_1.X2 >= rect_2.X1 and rect_1.Y1 <= rect_2.Y2 and rect_1.X1 <= rect_2.X2):
+			print("oh my gahhhh")
+
+			
+rect_1 = rekt(37,50,4,76)
+
+rect_2 = rekt(-77,-790,-876,-87)
+rect_1.getrekt()
+rect_2.getrekt()
+
+rect_1.coco(rect_1,rect_2)
+
+
+
+
+
+
+mainloop()
