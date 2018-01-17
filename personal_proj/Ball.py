3d492a7d923540e903c27c8d0da60d4b5a652b61
basel19-meet <basel19@meet.mit.edu>
2018-01-17 19:20:44 +0200

    ece

---
+++ personal_proj/Ball.py
@@ -0,0 +1,330 @@
+from turtle import*
+
+import random
+import time
+colormode(255)
+tracer(0)
+ht()
+
+class my_Ball(Turtle):   
+	def __init__(self,x,y,dx,dy,rad,color,shape):
+		Turtle.__init__(self)
+		self.x = x
+		self.y = y
+		self.dx = dx
+		self.dy = dy
+		self.rad = radius
+		self.shape("circle")
+		r = random.randint(0,255)
+		g = random.randint(0,255)
+		b = random.randint(0,255)
+		self.color(r,g,b)
+		
+
+	def move(self,screen_height,screen_width):
+		current_x = self.xcor()
+		current_y = self.ycor()
+		new_x = current_x+self.dx
+		new_y = current_y+self.dy
+		right_side_ball = new_x + self.rad
+		left_side_ball = new_x - self.rad
+		top_ball = new_y + self.rad
+		bottom_ball = new_y - self.rad
+		self.goto(new_x,new_y)
+		if(right_side_ball >= screen_width):
+			self.dx = -self.dx
+
+		if(left_side_ball <=(-screen_width)):
+			self.dx = -self.dx
+
+		
+		if(top_ball>=screen_height):
+			self.dy = -self.dy
+
+		if(bottom_ball<=(-screen_height)):
+			self.dy = -self.dy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		mainloop()
---
+++ personal_proj/agario.py
@@ -0,0 +1,6 @@
+from turtle import*
+from Ball import my_Ball
+import random
+import time
+ball = Ball(11,11,4,4,20)
+turtle.ht()
